9e64c3ed90cada2b7dd32461ffdc60c5aafd312a
Daniel Kuroski <daniel.kuroski@gmail.com>
2017-02-15 14:04:30 -0200

    Added entities and migrations

--- database/migrations/2017_02_15_152519_create_organizations_table.php
+++ database/migrations/2017_02_15_152519_create_organizations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateOrganizationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('phone');
-            $table->enum('roles', ['admin', 'editor', 'supporter']);
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -32,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organizations');
     }
 }
