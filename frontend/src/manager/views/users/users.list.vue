5f921b82458fad3bb8343a73de418a4fe5d898f9
Pedro Henrique Mognon <phm1993@gmail.com>
2017-09-14 00:16:37 -0300

    Refining component

--- frontend/src/manager/views/users/users.list.vue
+++ frontend/src/manager/views/users/users.list.vue
@@ -1,42 +1,21 @@
 <template>
     <div>
-        <h1>Users List</h1>
-
-        <!--<div class="datatable">-->
-            <!--<div class="datatable__header">-->
-              <!--<div class="datatable__column-name" v-for="columnName in header">-->
-                <!--{{columnName.name}}-->
-              <!--</div>-->
-            <!--</div>-->
-            <!--<div class="datatable__row" v-for="row in data">-->
-                <!--<div class="datatable__cell">-->
-                  <!--{{user.name}}-->
-                <!--</div>-->
-                <!--<div>-->
-                  <!--<button type="button" @click="deleteUser(user.id)">Delete</button>-->
-                <!--</div>-->
-            <!--</div>-->
-        <!--</div>-->
-
+      <h1>Users List</h1>
       <div class="datatable-counter">
         15 modelos de fluxo | 5 abertos | 0 fechados
       </div>
       <table class="datatable">
         <thead class="datatable__header">
           <tr>
-            <th>Nome do modelo</th>
-            <th>Última atualização</th>
-            <th>Status</th>
-            <th></th>
+            <th v-for="columnName in header">{{columnName}}</th>
+            <th>Ações</th>
           </tr>
         </thead>
         <tbody class="datatable__body">
-          <tr class="animated fadeIn">
-            <td data-title="Nome do modelo">Solicitação de manutenção</td>
-            <td data-title="Última atualização">21/12/2016 às 14:31</td>
-            <td data-title="Status">3 fluxos abertos, 1 fechado e 1 pendente</td>
+          <tr class="animated fadeIn" v-for="user in userList">
+            <td data-title="Nome">{{user.name}}</td>
             <td>
-              <button class="datatable__delete-btn"><i class="fa fa-trash"></i></button>
+              <button class="datatable__delete-btn" @click="deleteUser(user.id)"><i class="fa fa-trash"></i></button>
             </td>
           </tr>
         </tbody>
@@ -53,15 +32,11 @@
         },
         data() {
             return {
-                userList : [
-                    {
-                        name: 'Pedro'
-                    },
-                    {
-                        name: 'João'
-                    }
-                ],
-              requestService: new RequestService()
+                userList: [],
+                requestService: new RequestService(),
+                header: [
+                	'Nome',
+                ]
             }
         },
         methods: {
