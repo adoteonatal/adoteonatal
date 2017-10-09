5aaa690f9494a807eb2c066128420f4767d88045
Daniel Kuroski <daniel.kuroski@gmail.com>
2017-10-08 21:04:56 -0300

    Finished class tests

--- test/integration/api/v1/class.spec.js
+++ test/integration/api/v1/class.spec.js
@@ -45,6 +45,19 @@ suite('Classes', () => {
       res.body.day_care.should.be.equal(dayCare);
     });
 
+    test('should fail to create a new class when day_care is undefined', async () => {
+      const name = faker.internet.userName();
+
+      const res = await request(app)
+        .post('/v1/classes')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ name })
+        .expect(500);
+
+      res.body.should.be.an('object');
+      res.body.error.should.be.equal('ValidationError: day_care: Class [day_care] field is required');
+    });
+
     test('should fail to create a new class when name is undefined', async () => {
       const dayCare = '59c6a58534a4050de012f617';
 
@@ -69,6 +82,67 @@ suite('Classes', () => {
 
       res.body.should.be.an('object');
       res.body.error.should.be.equal('ValidationError: name: Class [name] field is required');
+    });
+  });
+
+  suite('#delete', () => {
+    test('should delete an class', async () => {
+      const name = faker.internet.userName();
+      const dayCare = '59c6a58534a4050de012f617';
+
+      const classInsert = await request(app)
+        .post('/v1/classes')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ name, day_care: dayCare })
+        .expect(201);
+
+      const res = await request(app)
+        .delete(`/v1/classes/${classInsert.body._id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(204);
+
+      res.body
+        .should.be.an('object')
+        .that.is.empty;
+    });
+
+    test('should succeed even when class does not exist', async () => {
+      const res = await request(app)
+        .delete('/v1/classes/598ce31fd826193bbe675726')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(204);
+
+      res.body
+        .should.be.an('object')
+        .that.is.empty;
+    });
+  });
+
+  suite('#update', () => {
+    test('should update an class with some variables', async () => {
+      const expectName = faker.internet.userName();
+      const name = faker.internet.userName();
+
+      const dayCareInsert = await request(app)
+        .post('/v1/day-cares')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ name })
+        .expect(201);
+
+      const classInsert = await request(app)
+        .post('/v1/classes')
+        .set('Authorization', `Bearer ${token}`)
+        .send({ name, day_care: dayCareInsert.body._id })
+        .expect(201);
+
+      const res = await request(app)
+        .put(`/v1/classes/${classInsert.body._id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({ name: expectName })
+        .expect(200);
+
+      res.body.should.be.an('object');
+      res.body.name.should.be.equal(expectName);
     });
   });
 
